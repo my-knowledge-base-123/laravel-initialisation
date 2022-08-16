1f266cfd9257a848e2a334edc17d3370fe768fb2
Gary Zhang <gary.zhang@lifebyte.io>
2022-08-16 17:35:07 +1000

    WEB-628 #comment update: seeders

---
+++ src/database/seeders/UserSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    public function run(): void
+    {
+        User::factory(10)->create();
+    }
+}
